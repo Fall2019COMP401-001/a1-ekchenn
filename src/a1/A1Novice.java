a912b0440279d0c6b39ac394832beb1f0809a55f
ericakaren <ericakaren@ericas-mbp.wireless-1x.unc.edu>
2019-08-28 14:13:03 -0400

    First try?

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -7,8 +7,24 @@ public class A1Novice {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
 		
+
+		int total_cust = scan.nextInt();
+		String first_name = scan.nextLine();
+		String last_name = scan.nextLine();
+		int total_items = scan.nextInt();
+		
+		double amt_spent = 0.0;
+				
+		for (int i = 0; i < total_items; i++) 
+		{
+			int item_quantity = scan.nextInt();
+			String item_name = scan.nextLine();
+			double item_price = scan.nextDouble();
+			
+			amt_spent = amt_spent + (item_quantity * item_price);
+		}
+		
+		System.out.println(first_name.charAt(0) + ". " + last_name + ": " + amt_spent);
 	}
 }
